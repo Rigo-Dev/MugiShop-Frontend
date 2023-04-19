a7bfb41543474f3947a607478b97ba12f4fb5464
Rigo-Dev <juandiaz031317@gmail.com>
2023-04-19 08:22:44 -0400

    Validar datos desde el frontend Login/Register

---
+++ pages/Register.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import "../styleSheets/Register.css"
+import { ZodType, z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Nav } from '../src/components/Nav'
+import { NavDropDown } from '../src/components/NavDropDown'
+import { NavLink } from 'react-router-dom'
+
+export function Register() {
+
+    type FormData = {
+        first_name: string,
+        last_name: string,
+        email: string,
+        password: string,
+    }
+
+    const schema: ZodType<FormData> = z.object({
+        first_name: z.string(),
+        last_name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(8).max(100)
+    })
+
+
+    const {register, handleSubmit, formState: { errors }} = useForm<FormData>({resolver: zodResolver(schema)})
+
+    async function submitData(data: FormData){
+        const res = await fetch("http://localhost:8000/api/register", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+
+        const UserData = await res.json()
+
+        console.log(UserData);
+
+        if (UserData.access_token) {
+            console.log("Registrado");
+            sessionStorage.setItem("token", UserData.access_token)
+        }
+      
+    } 
+
+  return (
+    <div className="main_container_register">
+      <Nav />
+      <NavDropDown />
+      <div className='container_register'>
+        <div className="register">        
+          <h1 className="title">Register</h1>
+          <div className="container_form">
+            <form action="" className='form' onSubmit={handleSubmit(submitData)}>
+                <input type="text" className='input_form' placeholder='name' {...register("first_name")}/>
+                {errors.first_name && <span>{errors.first_name.message}</span>}
+                <input type="text" className='input_form' placeholder='last Name' {...register("last_name")}/>
+                {errors.last_name && <span>{errors.last_name.message}</span>}
+                <input type="text" className='input_form' placeholder='email' {...register("email")}/>
+                {errors.email && <span>{errors.email.message}</span>}
+                <input type="text"className='input_form' placeholder='password' {...register("password")}/>
+                {errors.password && <span>{errors.password.message}</span>}
+                  <div className="options_singin"> 
+                    <button type="submit" className="button_singin">Sing In</button>
+                      <h5>You're not login?</h5>
+                      <NavLink className={"redirect_singup"} to={'/login'}>Sing In</NavLink>
+                  </div>
+            </form>
+          </div>
+         </div>   
+      </div>
+    </div>
+  )
+}
+
+
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import "../../styleSheets/Products.css"
+import "../../styleSheets/Product.css"
 import { useState } from "react";
 import { AiOutlineShoppingCart, AiOutlineHeart, AiFillHeart, AiTwotoneShopping } from "react-icons/ai";
 
 export function Products({ nameProduct, imageProduct, idProduct, priceProduct }) {
-  const [liked, setLiked] = useState(false);
-
-  const handleLikeClick = () => {
-    setLiked(!liked);
-  };
-
   const [shopping, setShopping] = useState(false)
 
   const handleShoppingClick = () =>{
     setShopping(!shopping)
   }
+
+  const [liked, setLiked] = useState(false);
+
+  const handleLikeClick = () => {
+    setLiked(!liked);
+  };
 
   const url = "https://mugishop-miniproyecto.s3.amazonaws.com"
 
@@ -22,8 +22,8 @@ export function Products({ nameProduct, imageProduct, idProduct, priceProduct })
     <div className="product_container">
       <h1 className="name_product">{nameProduct}</h1>
       <img className="image" src={url + imageProduct} alt={idProduct} />
-      <div className="price_product">${priceProduct}</div>
         <div className="options_product">
+        <h4 className="price_product">${priceProduct}</h4>
             {shopping ? (
               <AiTwotoneShopping className="shopping cart-shoppin" onClick={handleShoppingClick} />
             ): (
