627fe075fcd4f014f219f77a61a2a4f6b5c0b921
Rigo-Dev <juandiaz031317@gmail.com>
2023-06-09 17:57:59 -0400

    Descargar imagenes compradas

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import "../../styleSheets/Profile.css";
 import { Nav } from "../components/Nav";
 import { FechtProfile } from "../../utils/ProfileFunctions";
 import { NavLink } from "react-router-dom";
+import { AiOutlineDownload } from "react-icons/ai";
 
 export function Profile() {
   const [DataProfile, setDataProfile] = useState([]);
@@ -13,6 +14,20 @@ export function Profile() {
     console.log(data);
     setDataProfile(data);
   };
+
+  function dowloadImg(img) {
+    const url = img;
+    fetch(url)
+      .then((res) => res.blob())
+      .then((blob) => {
+        const a = document.createElement("a");
+        a.href = window.URL.createObjectURL(blob);
+        a.download = "image.png";
+        a.click();
+
+        console.log(blob);
+      });
+  }
 
   const getImages = async () => {
     const token = sessionStorage.getItem("token");
@@ -54,11 +69,21 @@ export function Profile() {
               </NavLink>
             </div>
           </div>
-          <div className="image_profile">
+          <div className="ntf_user_container">
             {images.length >= 1
               ? images.map((p) => (
                   <div className="columns_profile" key={p.id}>
-                    <img className="image" src={url + p.image} alt="" />
+                    <div className="nft_user">
+                      <img className="image" src={url + p.image} alt="" />
+                      <div className="download_option_container">
+                        <div className="download_option">
+                          <AiOutlineDownload className="button_download" onClick={() => dowloadImg(url + p.imagehd)}>
+                            Dowload
+                          </AiOutlineDownload>
+                        </div>
+
+                      </div>
+                    </div>
                   </div>
                 ))
               : null}
